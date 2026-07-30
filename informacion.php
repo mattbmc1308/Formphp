2fe71065855259ce33004038cd4de93ef5b4f746
mattbmc1308 <bmchristian16@gmail.com>
2026-07-30 15:25:04 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,19 @@
+<?php
 
+$Identificacion = $_POST['Identificacion'];
+$NomApellidos = $_POST['NomApellidos'];
+$Cargo = $_POST['Cargo'];
+$Salario = $_POST['Salario'];
+
+echo "La identificación del empleado es: " . $Identificacion;
+echo "<br>";
+
+echo "Los nombres y apellidos del empleado son: " . $NomApellidos;
+echo "<br>";
+
+echo "El cargo del empleado es: " . $Cargo;
+echo "<br>";
+
+echo "El salario del empleado es: " . $Salario;
+
+?>
